4c8a86465394258af3ab50787c1060f14a01693f
otowmoyarng <hidemusiam@gmail.com>
2020-07-05 11:38:55 +0900

    日付の戻り値を追加

--- 20200705_pythonRestAPI_sample/restapi_sample.py
+++ 20200705_pythonRestAPI_sample/restapi_sample.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-import datetime
+from datetimeutils import DateTimeUtils
 
 app = Flask(__name__)
 
 @app.route('/hello')
-def hello():
+def hello() -> str:
     return "Hello World"
 
 @app.route('/today')
-def today():
-    return datetime.date.today().strftime("%Y/%m/%d")
+def today() -> str:
+    return DateTimeUtils.responcedate(0)
+
+@app.route('/tomorrow')
+def tomorrow() -> str:
+    return DateTimeUtils.responcedate(1)
+
+@app.route('/yesterday')
+def yesterday() -> str:
+    return DateTimeUtils.responcedate(-1)
 
 if __name__ == "__main__":
     app.run()
