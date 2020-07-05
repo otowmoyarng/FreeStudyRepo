fbf821f0082c27648d0c91083a2ebcc1e7980a5f
otowmoyarng <hidemusiam@gmail.com>
2020-07-05 11:04:48 +0900

    today追加

--- 20200705_pythonRestAPI_sample/restapi_sample.py
+++ 20200705_pythonRestAPI_sample/restapi_sample.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     return "Hello World"
+
+@app.route('/today')
+def today():
+    return datetime.date.today().strftime("%Y/%m/%d")
 
 if __name__ == "__main__":
     app.run()
