5f9429aa7e578a67c28ae8641214dc9c154609d7
hidemusiam <hidemusiam@gmail.com>
2020-09-26 08:38:13 +0900

    ダウンロードしたファイルのチェック追加

--- 20200924_pdfload/source/download.py
+++ 20200924_pdfload/source/download.py
@@ -1,25 +1,35 @@
+import os
 import requests
 import shutil
 
-def download(url : str):
+def download(url : str, downloadfile : str):
     """[summary]
     指定したurlからファイルをダウンロードする
     Args:
-        url ([str]): ダウンロード先url
+        url ([str]): ダウンロード先のurl
+        downloadfile ([str]): ダウンロードファイルのフルパス
     Returns:
         ダウンロードしたファイル
     """
     if url is None:
         print('URLが指定されていない')
-    else:
-        try:
-            r = requests.get(url, stream=True)
-            with open('./20200924_pdfload/downloaded.pdf', mode='wb') as dfile:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, dfile)
-        except requests.exceptions.RequestException as ex:
-            print(ex)
-            raise ex
+        return
+    
+    if os.path.exists(downloadfile):
+        print('ファイルが既にダウンロードされている')
+        return
+
+    try:
+        r = requests.get(url, stream=True)
+        with open(downloadfile, mode='wb') as dfile:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, dfile)
+    except requests.exceptions.RequestException as ex:
+        print(ex)
+        raise ex
 
 if __name__ == "__main__":
-    download('https://www.npa.go.jp/safetylife/seianki/jisatsu/R02/202009sokuhouti.pdf')
+    print(os.getcwd())
+    url = 'https://www.npa.go.jp/safetylife/seianki/jisatsu/R02/202009sokuhouti.pdf'
+    downloadfile = os.getcwd() + '\\20200924_pdfload\\downloaded.pdf'
+    download(url, downloadfile)
