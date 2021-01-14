b7c50b359b2ab034d8f189775a826eae3c4a22b2
hidemusiam <hidemusiam@gmail.com>
2021-01-15 00:04:19 +0900

    a tour of go slice finished

--- 20210112_atourofgo/sample.go
+++ 20210112_atourofgo/sample.go
@@ -6,6 +6,7 @@ import (
 	"math/cmplx"
 	"math/rand"
 	"runtime"
+	"strings"
 	"time"
 )
 
@@ -43,6 +44,22 @@ func main() {
 	switch4()
 	defer1()
 	defer2()
+
+	/* moretypes */
+	pointers()
+	structs1()
+	structs2()
+	structs3()
+	array1()
+	slice1()
+	slice2()
+	slice3()
+	slice4()
+	slice5()
+	slice6()
+	slice7()
+	slice8()
+	slice9()
 
 	fmt.Println(" ")
 	fmt.Println("a Tour of Go 終了")
@@ -366,4 +383,230 @@ func defer2() {
 		defer fmt.Println(i)
 	}
 	fmt.Println("done")
+}
+
+/* moretypes */
+func pointers() {
+	fmt.Println(" ")
+	fmt.Println("1.pointers")
+	i, j := 42, 2701
+
+	p := &i
+	fmt.Println("p:", *p)
+	*p = 21
+	fmt.Println("i:", i)
+	fmt.Println("p:", p)
+
+	p = &j
+	*p = *p / 37
+	fmt.Println("j:", j)
+	fmt.Println("p:", p)
+}
+
+type Vertex struct {
+	X, Y int
+}
+
+func structs1() {
+	fmt.Println(" ")
+	fmt.Println("2.structs1")
+	fmt.Println(Vertex{1, 2})
+}
+
+func structs2() {
+	fmt.Println(" ")
+	fmt.Println("3.structs2")
+	var v Vertex = Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
+}
+
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	p  = &Vertex{1, 2}
+)
+
+func structs3() {
+	fmt.Println(" ")
+	fmt.Println("4.structs3")
+	fmt.Println(v1, v2, v3, p)
+}
+
+func array1() {
+	fmt.Println(" ")
+	fmt.Println("5.array1")
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
+
+func slice1() {
+	fmt.Println(" ")
+	fmt.Println("6.slice1")
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+}
+
+func slice2() {
+	fmt.Println(" ")
+	fmt.Println("7.slice2")
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
+}
+
+func slice3() {
+	fmt.Println(" ")
+	fmt.Println("8.slice3")
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
+}
+
+func slice4() {
+	fmt.Println(" ")
+	fmt.Println("9.slice4")
+	s := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(s)
+
+	t := s[1:4]
+	fmt.Println(t)
+
+	t = s[:2]
+	fmt.Println(t)
+
+	t = s[1:]
+	fmt.Println(t)
+
+	t = s[:]
+	fmt.Println(t)
+}
+
+func slice5() {
+	fmt.Println(" ")
+	fmt.Println("10.slice5")
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	s = s[:0]
+	printSlice(s)
+
+	s = s[:4]
+	printSlice(s)
+
+	s = s[2:]
+	printSlice(s)
+}
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+func slice6() {
+	fmt.Println(" ")
+	fmt.Println("11.slice6")
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
+}
+
+func slice7() {
+	fmt.Println(" ")
+	fmt.Println("12.slice7")
+
+	a := make([]int, 5)
+	printSlice2("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice2("b", b)
+
+	c := b[:2]
+	printSlice2("c", c)
+
+	d := c[2:5]
+	printSlice2("d", d)
+}
+func printSlice2(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
+
+func slice8() {
+	fmt.Println(" ")
+	fmt.Println("13.slice8")
+
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}
+
+func slice9() {
+	fmt.Println(" ")
+	fmt.Println("14.slice9")
+
+	var s []int
+	printSlice3(s)
+
+	s = append(s, 0)
+	printSlice3(s)
+
+	s = append(s, 1)
+	printSlice3(s)
+
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+func printSlice3(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
