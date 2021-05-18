d87f73f4a05a3b590c41838df72464f23d318726
otowmoyarng <hidemusiam@gmail.com>
2021-05-18 23:05:01 +0900

    疎通テスト用のエンドポイントを追加

--- 20210518_gcpstudy/fastapi_gae/gae/main.py
+++ 20210518_gcpstudy/fastapi_gae/gae/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+import os
+
+
+apiVersion = os.getenv("API_VERSION", "1.0.0")
 
 app = FastAPI(
     title="FastAPISample",
     description="GoogleAppEngineで起動するFastAPIのサンプルです。",
-    version="1.0.0"
+    version=apiVersion
 )
 
 
 @app.get("/")
 async def root():
-    return {"hello": "world"}
+    return {
+        "hello": "world"
+    }
+
+
+@app.get("/test")
+async def test(param: str = "NoParam"):
+    return {
+        "code": "200",
+        "param": param,
+        "version": apiVersion
+    }
