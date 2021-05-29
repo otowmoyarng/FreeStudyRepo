8d25829e1697dfea377df8be1e06f6b2e51ebc39
otowmoyarng <hidemusiam@gmail.com>
2021-05-29 12:53:08 +0900

    C#のラムダ式のサンプルコードを書いてみた

---
+++ 20210529_CSharpePractice/ConsoleApp/main.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp.Functions;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Lambda la = new Lambda();
+            la.Do();
+        }
+    }
+}
