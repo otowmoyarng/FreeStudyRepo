fd5c7eff83de7291b7894c56f713cdbf9add104a
otowmoyarng <hidemusiam@gmail.com>
2022-07-05 23:05:31 +0900

    20220224_CostcoNotify削除

--- 20220224_CostcoNotify/test/GASProperties.js
+++
@@ -1,6 +0,0 @@
-/**
- * GASProperties用テスト関数
- */
-function TestGetProperty() {
-    console.log(`key:${GASPropertiesKey.AccessToken},value:${GASProperties.GetProperty(GASPropertiesKey.AccessToken)}`);
-}
